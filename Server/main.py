8f45eba64e58822ef8ac0f46274150ebb40638ea
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2026-03-01 19:04:02 +0530

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-import google.generativeai as genai
-genai.configure(api_key='AIzaSyAMjsZilyZXxmG3mVDmqb6Y4D30ZX-GwNs')
+from google import genai
+
+client = genai.Client(api_key='YOUR_API_KEY')
 
 app = Flask(__name__)
 CORS(app)
-model = genai.GenerativeModel('gemini-pro')
 
 @app.route("/", methods=["POST"])
 def index():
     data = request.get_json()
-    response = model.generate_content(data.get("prompt"))
-    return (response.text)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash-native-audio-preview-09-2025',
+        contents=data.get("prompt")
+    )
+    return response.text
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     app.run(debug=True)
